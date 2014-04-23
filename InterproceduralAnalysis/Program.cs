d275c2e1e99ad4542281720ae3760ae16ba83825
Petr Baroš <pbaros@gmail.com>
2014-04-23 21:12:58 +0200

    Spousteni  programu s argumenty

--- InterproceduralAnalysis/Program.cs
+++ InterproceduralAnalysis/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -20,19 +21,24 @@ namespace InterproceduralAnalysis
 
         static int Main(string[] args)
         {
-            //programName = args[0];
-            programFile = @"d:\Projects\github\InterproceduralAnalysis\InterproceduralAnalysis\program.txt";
-            //printLA = arg[1];
+            programFile = null;
             printLA = false;
             printSA = false;
-            printIAM = true;
-            printIAG = true;
-            printIALE = true;
+            printIAM = false;
+            printIAG = false;
+            printIALE = false;
+
+            if (!ParseArguments(args))
+            {
+                PrintHelp();
+                Console.ReadKey();
+                return -1;
+            }
 
             if (programFile == null)
             {
                 Console.WriteLine("Parametr 'programFile' je povinny.");
-                // PrintHelp();
+                PrintHelp();
                 Console.ReadKey();
                 return -1;
             }
@@ -40,7 +46,7 @@ namespace InterproceduralAnalysis
             if (!File.Exists(programFile))
             {
                 Console.WriteLine(string.Format("Soubor programu '{0}' neexistuje.", programFile));
-                // PrintHelp();
+                PrintHelp();
                 Console.ReadKey();
                 return -1;
             }
@@ -69,17 +75,140 @@ namespace InterproceduralAnalysis
             GraphGenerator gg = new GraphGenerator();
             gg.CreateGraph(prg);
 
+            Stopwatch s = new Stopwatch();
+            s.Start();
+
             int w = 3;
             int n = prg.VarsDecl.Count;
             InterproceduralAnalyzer ia = new InterproceduralAnalyzer(w, n, printIAM, printIAG, printIALE);
             ia.Analyze(prg);
 
+            s.Stop();
+
             WriteProgram wp = new WriteProgram();
             wp.Write(prg, programFile);
 
-            Console.WriteLine("Konec analyzy... stiskni klavesu.");
+            Console.WriteLine("Cas analyzy: {0:N3} s", s.ElapsedMilliseconds / 1000.0);
+            Console.WriteLine("Stiskni klavesu...");
             Console.ReadKey();
             return 0;
+        }
+
+        static bool ParseArguments(string[] args)
+        {
+            int qi = Array.FindIndex(args, x => x == "/?");
+            if (qi >= 0)
+            {
+                return false;
+            }
+
+            int ai = 0;
+            bool isProgram = false;
+
+            while (ai < args.Length)
+            {
+                if (args[ai] == "/p")
+                {
+                    if (isProgram)
+                    {
+                        Console.WriteLine("Parametr jmeno souboru programu musi byt pouze jednou.");
+                        Console.WriteLine();
+                        return false;
+                    }
+
+                    if (ai >= (args.Count() - 1))
+                    {
+                        Console.WriteLine("Parametr jmeno souboru programu je povinny.");
+                        Console.WriteLine();
+                        return false;
+                    }
+
+                    programFile = args[ai + 1];
+                    isProgram = true;
+
+                    ai += 2;
+                    continue;
+                }
+
+                if (args[ai] == "/d")
+                {
+                    if (ai >= (args.Count() - 1))
+                    {
+                        Console.WriteLine("Volitelny parametr zobrazeni musi specifikovat volby.");
+                        Console.WriteLine();
+                        return false;
+                    }
+
+                    string[] dos = args[ai + 1].Split('|');
+                    foreach (string doi in dos)
+                    {
+                        switch (doi)
+                        {
+                            case "la":
+                                printLA = true;
+                                break;
+                            case "sa":
+                                printSA = true;
+                                break;
+                            case "iam":
+                                printIAM = true;
+                                break;
+                            case "iag":
+                                printIAG = true;
+                                break;
+                            case "iale":
+                                printIALE = true;
+                                break;
+                            case "all":
+                                printLA = true;
+                                printSA = true;
+                                printIAM = true;
+                                printIAG = true;
+                                printIALE = true;
+                                break;
+                            default:
+                                Console.WriteLine("Neznama volba zobrazeni '{0}'", doi);
+                                Console.WriteLine();
+                                return false;
+                        }
+                    }
+
+                    ai += 2;
+                    continue;
+                }
+
+                Console.WriteLine("Neznamy parametr '{0}'.", args[ai]);
+                Console.WriteLine();
+                return false;
+            }
+
+            return true;
+        }
+
+        static void PrintHelp()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Pouziti:");
+            Console.WriteLine("    intprocan.exe /p <jmeno_program> [ /? |");
+            Console.WriteLine("                                       /d [all | la | sa | iam | iag | iale]");
+            Console.WriteLine();
+            Console.WriteLine("Kde:");
+            Console.WriteLine("    /p <jmeno_program>    povinny parametr - jmeno programu");
+            Console.WriteLine("Volitelne:");
+            Console.WriteLine("    /?                    zobrazeni napovedy");
+            Console.WriteLine("    /d                    zobrazeni pomocnych vypisu");
+            Console.WriteLine("    /d all                zobrazeni vsech pomocnych vypisu");
+            Console.WriteLine("    /d la                 zobrazeni pomocnych vypisu lexikalni analyzy");
+            Console.WriteLine("    /d sa                 zobrazeni pomocnych vypisu syntakticke analyzy");
+            Console.WriteLine("    /d iam                zobrazeni pomocnych vypisu interproceduralni analyzy vypoctu prechodovych matic");
+            Console.WriteLine("    /d iag                zobrazeni pomocnych vypisu interproceduralni analyzy vypoctu generatoru");
+            Console.WriteLine("    /d iale               zobrazeni pomocnych vypisu interproceduralni analyzy vypoctu linearnich rovnic");
+            Console.WriteLine();
+            Console.WriteLine("Priklady:");
+            Console.WriteLine("    > intprocan.exe /p program.txt");
+            Console.WriteLine("    > intprocan.exe /p C:\\project\\program.txt /d all");
+            Console.WriteLine("    > intprocan.exe /p C:\\project\\program.txt /d la|sa|iale");
+            Console.WriteLine();
         }
     }
 }
